3f5a2e12e98c2e1530f17cd093585b51cbf309b7
CameraSkunk <daniel.d.coelho@gmail.com>
2025-11-21 18:53:11 -0500

    Chapter 3: Lesson 2

--- asteroids/main.py
+++ asteroids/main.py
@@ -4,12 +4,14 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    Clock = pygame.time.Clock()
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -18,11 +20,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-        dt = Clock.tick(60) / 1000 #sets frame rate and updates dt (delta time) since last call
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
