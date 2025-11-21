c0f9f5ad331b783ca2109ad97dd707f9bfdf8430
CameraSkunk <daniel.d.coelho@gmail.com>
2025-11-21 08:08:34 -0500

    Chapter 2: Lesson 4

--- asteroids/main.py
+++ asteroids/main.py
@@ -9,6 +9,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    Clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
 
@@ -19,6 +22,7 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
+        dt = Clock.tick(60) / 1000 #sets frame rate and updates dt (delta time) since last call
 
 if __name__ == "__main__":
     main()
