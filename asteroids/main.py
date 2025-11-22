f14f2c6ce4a61ff193697d6b15cc677edff54ab9
CameraSkunk <daniel.d.coelho@gmail.com>
2025-11-22 09:59:52 -0500

    Chapter 4: Lesson 1

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,7 +1,10 @@
+#Run "source .venv/bin/activate" in the asteroids folder
 #Run test using "uv run main.py" on command line.
 #use Ctrl+c in terminal to kill game
 
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
@@ -11,12 +14,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-        
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
-    
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
@@ -31,8 +39,10 @@ def main():
         updatable.update(dt)
 
         screen.fill("black")
+
         for obj in drawable:
             obj.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
